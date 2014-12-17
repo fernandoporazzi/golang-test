4f2cb270ad234f7c91fafcf06425c4e4128e0d34
Fernando Porazzi <fporazzi46@gmail.com>
2014-12-16 23:30:27 -0200

    Convert tabs to spaces

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"html/template"
-	"net/http"
-	"time"
+  "html/template"
+  "net/http"
+  "time"
 )
 
 func main() {
-	setStaticDir()
+  setStaticDir()
 
-	http.HandleFunc("/", index)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/service", service)
-	http.ListenAndServe(":8080", nil)
+  http.HandleFunc("/", index)
+  http.HandleFunc("/about", about)
+  http.HandleFunc("/service", service)
+  http.ListenAndServe(":8080", nil)
 }
 
 func setStaticDir() {
@@ -22,64 +22,63 @@ func setStaticDir() {
 
 // Available templates.
 var templates = template.Must(template.ParseFiles(
-	"templates/index.html",
-	"templates/about.html",
-	"templates/service.html",
+  "templates/index.html",
+  "templates/about.html",
+  "templates/service.html",
 ))
 
 // data needed to render the template
-
 type About struct {
-	Description string
+  Description string
 }
 
 type Service  struct {
-	Services string
+  Services string
 }
 
 type Page struct {
-	About       *About
-	Service     *Service
-	Title  			string
-	CurrentYear string
+  About       *About
+  Service     *Service
+  Title       string
+  CurrentYear string
 }
 
 // Render templates.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+  err := templates.ExecuteTemplate(w, tmpl+".html", p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
 
 // Handler to show the index page
 func index(w http.ResponseWriter, r *http.Request) {
-	year := time.Now().Local()
-	page := Page{
-		Title:       "Home",
-		CurrentYear: year.Format("2006"),
-	}
-	renderTemplate(w, "index", &page)
+  year := time.Now().Local()
+  page := Page{
+    Title:       "Home",
+    CurrentYear: year.Format("2006"),
+  }
+  renderTemplate(w, "index", &page)
 }
 
 // Handler to show about page
 func about(w http.ResponseWriter, r *http.Request) {
-	about := Page{
-		Title: "About",
-		About: &About{
-				Description: "lorem ipsum",
-			},
-	}
-	renderTemplate(w, "about", &about)
+  about := Page{
+    Title: "About",
+    About: &About{
+        Description: "lorem ipsum",
+      },
+  }
+  renderTemplate(w, "about", &about)
 }
 
 // Handler to show about page
 func service(w http.ResponseWriter, r *http.Request) {
-	service := Page{
-		Title: "About",
-		Service: &Service{
-				Services: "this should be a list where I name the things I do",
-			},
-	}
-	renderTemplate(w, "service", &service)
+  service := Page{
+    Title: "About",
+    Service: &Service{
+        Services: "this should be a list where I name the things I do",
+      },
+  }
+  renderTemplate(w, "service", &service)
 }
